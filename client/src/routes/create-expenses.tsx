695d2d159ba55838e25f4ded5cec5a5610d9400c
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-08-09 11:55:51 +0500

    removed auth files and errors

--- client/src/routes/create-expenses.tsx
+++ client/src/routes/create-expenses.tsx
@@ -1,22 +1,16 @@
 import { Input } from '@/components/ui/input'
 import { createFileRoute } from '@tanstack/react-router'
-import {useForm} from 'react-hook-form'
-import {z} from 'zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 export const Route = createFileRoute('/create-expenses')({
 	component: () => <CreateExpenses />,
 })
 
-
-
 function CreateExpenses() {
-const {register,formState:{errors},handleSubmit} = useForm({resolver:})
 	return (
 		<div>
-			<form>
-      <Input onChange={}/>
-      </form>
+			<form>this is a form</form>
 		</div>
 	)
 }
-
